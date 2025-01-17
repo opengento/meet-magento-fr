739991cc1674b88cf93553f00b095420e5b5ddbe
Timothée AUBRY <timothee.aubry@ph2m.com>
2025-01-17 19:32:00 +0100

    Header wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/layouts/Header/Header";
+import Container from "@/layouts/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Container size="large">
+          <Header />
+          <main>{children}</main>
+        </Container>
       </body>
     </html>
   );
