87cfec3ab8b93d9da493fcc6e45daedecb830c8b
✨ <hello@baptistebourzeix.fr>
2025-01-19 00:17:50 +0100

    Move types to an external file

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,18 +1,4 @@
-import { ReactNode } from 'react';
-
-type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'subtitle1' | 'subtitle2' | 'body1' | 'body2' | 'caption' | 'button' | 'overline';
-type Color = 'primary' | 'secondary' | 'success' | 'error' | 'warning' | 'info' | 'light' | 'dark';
-type Weight = 'thin' | 'light' | 'normal' | 'medium' | 'semibold' | 'bold' | 'extrabold';
-type Align = 'left' | 'center' | 'right' | 'justify';
-
-interface TypographyProps {
-    variant?: Variant;
-    color?: Color;
-    weight?: Weight;
-    align?: Align;
-    className?: string;
-    children: ReactNode;
-}
+import { TypographyProps } from "@/components/Typography/Typography.types";
 
 const Typography = ({
                         variant = 'body1',
