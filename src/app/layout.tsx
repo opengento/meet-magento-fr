612b23250f35bb0f07c0bbbe999d31fb41d13c3a
Thomas KLEIN <tomak@dnd.fr>
2025-01-23 19:11:31 +0100

    Add code-of-conduct & legal-terms pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <Container size="large">
           <Header />
           <main>{children}</main>
-            <Footer />
+          <Footer />
         </Container>
       </body>
     </html>
---
+++ src/app/legal-terms/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+      <section>
+          <h1>Legal Terms</h1>
+          <article>
+              <p>This website is edited by the association loi 1901 « Opengento » (RNA W833003136).</p>
+              <p>Represented by: Frédéric MARTINEZ.</p>
+              <p>Head office: 17 rue Idrac - 31000 Toulouse - France.</p>
+              <p>Contact: <a href="mailto:opengento@gmail.com">opengento@gmail.com</a></p>
+              <h2>Hosting</h2>
+              <p>This website is hosted with <em>♥</em> by GitHub.</p>
+              <h2>Cookies</h2>
+              <p>This website does not use any cookies and does not track any personal data.</p>
+          </article>
+      </section>
+  );
+}
