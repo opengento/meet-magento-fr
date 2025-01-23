d4667fca9231e7b6e796bcbae76a6d00d39a2dfe
Thomas KLEIN <tomak@dnd.fr>
2025-01-23 21:47:39 +0100

    Use proper component for rendering pages

--- src/app/legal-terms/page.tsx
+++ src/app/legal-terms/page.tsx
@@ -1,16 +1,45 @@
+import Typography from "@/components/Typography/Typography";
+import Link from "next/link";
+
 export default function Page() {
   return (
       <section>
-          <h1>Legal Terms</h1>
+          <Typography variant="h1" weight="bold" className="uppercase mb-4 text-white">
+              Legal Terms
+          </Typography>
+
           <article>
-              <p>This website is edited by the association loi 1901 « Opengento » (RNA W833003136).</p>
-              <p>Represented by: Frédéric MARTINEZ.</p>
-              <p>Head office: 17 rue Idrac - 31000 Toulouse - France.</p>
-              <p>Contact: <a href="mailto:opengento@gmail.com">opengento@gmail.com</a></p>
-              <h2>Hosting</h2>
-              <p>This website is hosted with <em>♥</em> by GitHub.</p>
-              <h2>Cookies</h2>
-              <p>This website does not use any cookies and does not track any personal data.</p>
+              <Typography className="text-white">
+                  This website is edited by the association loi 1901 « Opengento » (RNA W833003136).
+              </Typography>
+
+              <Typography className="text-white">
+                  Represented by: Frédéric MARTINEZ.
+              </Typography>
+
+              <Typography className="text-white">
+                  Head office: 17 rue Idrac - 31000 Toulouse - France.
+              </Typography>
+
+              <Typography className="text-white">
+                  Contact: <Link href="mailto:opengento@gmail.com">opengento@gmail.com</Link>
+              </Typography>
+
+              <Typography variant="h2" weight="bold" className="uppercase mb-4 text-white">
+                  Hosting
+              </Typography>
+
+              <Typography className="text-white">
+                  This website is hosted with <em>♥</em> by GitHub.
+              </Typography>
+
+              <Typography variant="h2" weight="bold" className="uppercase mb-4 text-white">
+                  Cookies
+              </Typography>
+
+              <Typography className="text-white">
+                  This website does not use any cookies and does not track any personal data.
+              </Typography>
           </article>
       </section>
   );
