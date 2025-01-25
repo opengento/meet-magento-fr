bc54439531a77fe261faf7bb40e1b360de339a70
Thomas KLEIN <tomak@dnd.fr>
2025-01-25 21:39:24 +0100

    Add properties so it can be overriden

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    output: "standalone",
+    basePath: "",
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
             use: ['@svgr/webpack']
         });
         return config;
-    }
+    },
 };
 
 export default nextConfig;
