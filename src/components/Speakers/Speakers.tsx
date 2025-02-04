56ca43bddbb62eba0b59a608d9f9f54390c5dda9
Timothée AUBRY <timothee.aubry@ph2m.com>
2025-02-04 16:42:23 +0100

    Fix border background

--- src/components/Speakers/Speakers.tsx
+++ src/components/Speakers/Speakers.tsx
@@ -11,7 +11,8 @@ const Speakers = ({ data }: { data: SpeakersProps }) => {
     <Container size="large">
       <div className="speakers flex flex-col py-8 md:py-12 gap-12">
         <SpeakersList speakers={data.speakers} />
-        <div className="flex flex-col md:flex-row bg-secondary pt-3 md:pl-3 md:pt-0 rounded-xl overflow-hidden">
+        <div className="flex flex-col md:flex-row rounded-xl overflow-hidden relative">
+          <div className="absolute inset-0 h-3 md:h-full md:w-3 bg-secondary" />
           <div className="bg-white flex-1 flex flex-col md:flex-row">
             <div className="flex flex-col gap-6 flex-1 py-8 px-6 md:py-12 md:px-16">
               <Typography
