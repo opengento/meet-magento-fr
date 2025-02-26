83ac53b25dc491f638abfffc0ace47a5a661242f
Thomas KLEIN <tomak@dnd.fr>
2025-02-26 21:57:20 +0100

    add link to place

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -41,13 +41,12 @@ const Hero = () => {
                 <div
                   className="flex h-10 gap-2 items-center rounded-full bg-white bg-opacity-20 py-2 px-6">
                   <CiCalendar size={22}/>
-                  <span className="font-medium">{t('hero:date')}</span>
+                  <Link href={'/place'} className="font-medium">{t('hero:date')}</Link>
                 </div>
                 <div
                   className="flex gap-2 items-center rounded-full bg-white bg-opacity-20 py-2 px-6">
                   <FaLocationDot size={20}/>
-                  <Link href={'/place'}
-                        className="font-medium">{t('hero:place')}</Link>
+                  <Link href={'/place'} className="font-medium">{t('hero:place')}</Link>
                 </div>
               </div>
             </div>
