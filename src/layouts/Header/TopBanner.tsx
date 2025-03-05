4243a1181464259551f59a5958506885f82ff96e
Thomas KLEIN <tomak@dnd.fr>
2025-03-05 15:44:43 +0100

    Adjust top banner

--- src/layouts/Header/TopBanner.tsx
+++ src/layouts/Header/TopBanner.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const TopBanner = () => {
   return (
     <div
-      className="w-full flex flex-col md:flex-row justify-center items-center py-1 px-4 gap-2 font-semibold bg-[#ff4859]">
-      <p>MMFR25 is available in two tracks:</p>
-      <p className="flex flex-row justify-center items-center gap-2">
+      className="w-full flex flex-col md:flex-row justify-center items-center py-1 px-4 gap-x-2 font-semibold bg-[#ff4859]">
+      <p>MMFR25 talks are available in two tracks:</p>
+      <p className="flex flex-row justify-center items-center gap-x-2">
         <Image
           src="/images/media/gb.svg"
           alt="gb"
