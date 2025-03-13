026d4a658386aa7671d6a00948837b236d5d3603
Thomas Klein <thomasklein876@gmail.com>
2025-03-13 23:29:36 +0100

    Fix top header color

--- src/layouts/Header/TopBanner.tsx
+++ src/layouts/Header/TopBanner.tsx
@@ -3,8 +3,7 @@ import React from "react";
 
 const TopBanner = () => {
   return (
-    <div
-      className="w-full flex flex-col md:flex-row justify-center items-center py-1 px-4 gap-x-2 font-semibold bg-[#ff4859]">
+    <div className="w-full flex flex-col md:flex-row justify-center items-center py-1 px-4 gap-x-2 font-semibold bg-[#ff4859] text-white">
       <p>MMFR25 talks are available in two tracks:</p>
       <p className="flex flex-row justify-center items-center gap-x-2">
         <Image
