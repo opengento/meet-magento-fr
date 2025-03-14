7ea19c73c09b174de966d467b1afce7b9400838d
Timothée AUBRY <timothee.aubry@ph2m.com>
2025-03-14 09:33:04 +0100

    setup program page

---
+++ src/app/program/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Typography from "@/components/Typography/Typography";
+import ContentMedia from "@/components/ContentMedia/ContentMedia";
+import Container from "@/layouts/Container";
+import { useTranslation } from "react-i18next";
+import React from "react";
+
+export default function Page() {
+  const { t } = useTranslation(["program"]);
+
+  return (
+    <Container size="large" className="flex flex-col gap-8 my-8">
+      <ContentMedia>
+        <Typography
+          variant="h2"
+          color="dark"
+          weight="semibold"
+          className="mb-2"
+        >
+          {t("program:title")}
+        </Typography>
+        <Typography color="dark" className="whitespace-pre-wrap">
+          {t("program:description")}
+        </Typography>
+      </ContentMedia>
+    </Container>
+  );
+}
