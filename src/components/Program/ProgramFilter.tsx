50f3936791fd3e8e9e491bf0309a4da9227e6b42
Timothée AUBRY <timothee.aubry@ph2m.com>
2025-03-20 10:38:07 +0100

    Add Program filter on page

--- src/components/Program/ProgramFilter.tsx
+++ src/components/Program/ProgramFilter.tsx
@@ -4,22 +4,55 @@ import { FaChartSimple } from "react-icons/fa6";
 import { FaHeart, FaStar, FaTag } from "react-icons/fa";
 import { BsFillHexagonFill } from "react-icons/bs";
 
-const ProgramFilter = () => {
-  const filters: { name: string; icon: React.ReactNode }[] = [
-    { name: "TOUT VOIR", icon: <MdDoubleArrow size={24} /> },
-    { name: "TECHNIQUE", icon: <FaChartSimple size={20} /> },
-    { name: "BUSINESS", icon: <FaTag size={20} /> },
-    { name: "IA", icon: <FaStar size={20} /> },
-    { name: "MAGENTO", icon: <BsFillHexagonFill size={20} /> },
-    { name: "PARCOURS", icon: <FaHeart size={20} /> },
+const ProgramFilter = ({
+  setActiveFilters,
+  activeFilters,
+}: {
+  setActiveFilters: React.Dispatch<React.SetStateAction<string[]>>;
+  activeFilters: string[];
+}) => {
+  const filters: { name: string; icon: React.ReactNode; value: string }[] = [
+    { name: "TOUT VOIR", icon: <MdDoubleArrow size={24} />, value: "all" },
+    {
+      name: "TECHNIQUE",
+      icon: <FaChartSimple size={20} />,
+      value: "Technical",
+    },
+    { name: "BUSINESS", icon: <FaTag size={20} />, value: "Business" },
+    { name: "IA", icon: <FaStar size={20} />, value: "IA" },
+    {
+      name: "MAGENTO",
+      icon: <BsFillHexagonFill size={20} />,
+      value: "magento",
+    },
+    { name: "PARCOURS", icon: <FaHeart size={20} />, value: "parcours" },
   ];
+  const handleFilterClick = (filter: string) => {
+    if (filter === "all") {
+      setActiveFilters([]);
+    } else {
+      const isSelected = activeFilters.includes(filter);
+      if (isSelected) {
+        setActiveFilters(activeFilters.filter((f) => f !== filter));
+      } else {
+        setActiveFilters([...activeFilters, filter]);
+      }
+    }
+  };
   return (
     <div className="program-filter flex justify-center items-center">
       <div className="flex gap-3 md:gap-6 bg-white rounded-full p-4 md:px-8 md:py-6 w-fit shadow-md">
         {filters.map((filter, index) => (
           <button
             key={index}
-            className="px-4 py-2 rounded-full bg-white text-primary border border-primary hover:border-primary hover:bg-primary hover:text-white"
+            onClick={() => handleFilterClick(filter.value)}
+            className={`px-4 py-2 rounded-full border border-primary 
+              ${
+                activeFilters.includes(filter.value) ||
+                (filter.value === "all" && activeFilters.length === 0)
+                  ? "bg-primary text-white"
+                  : "bg-white text-primary hover:bg-primary hover:text-white"
+              }`}
           >
             <div className="flex flex-row gap-1 items-center">
               {filter.icon}
